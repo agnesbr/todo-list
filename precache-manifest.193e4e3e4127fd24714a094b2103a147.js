27cc52c523067a2478e197c1c227f593cf1f2949
Agnes <agnes@MacBook-Pro-65.local>
2018-11-16 17:13:15 +0100

    Updates

---
+++ precache-manifest.193e4e3e4127fd24714a094b2103a147.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f47f260b12c7ad9f72e",
+    "url": "/todo-list/static/js/runtime~main.6f47f260.js"
+  },
+  {
+    "revision": "7f3485e7dcf55bb4b86c",
+    "url": "/todo-list/static/js/main.7f3485e7.chunk.js"
+  },
+  {
+    "revision": "29cc7fa31ac5bc93796c",
+    "url": "/todo-list/static/js/1.29cc7fa3.chunk.js"
+  },
+  {
+    "revision": "7f3485e7dcf55bb4b86c",
+    "url": "/todo-list/static/css/main.4a9bf463.chunk.css"
+  },
+  {
+    "revision": "665d304a42072716466035de888efa41",
+    "url": "/todo-list/index.html"
+  }
+];
